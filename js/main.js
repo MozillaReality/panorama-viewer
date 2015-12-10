3268d49ea4bddbf544d19930b9ac5ac7c3d8d689
caseyyee <caseyyee.ca@gmail.com>
2015-12-10 17:27:05 -0500

    fix whitespace

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // url parameters
 var parameters = (function() {
-	var parameters = {};
-	var parts = window.location.search.substr(1).split('&');
-	for (var i = 0; i < parts.length; i++) {
-		var parameter = parts[i].split('=');
-		parameters[parameter[0]] = parameter[1];
-	}
-	return parameters;
+  var parameters = {};
+  var parts = window.location.search.substr(1).split('&');
+  for (var i = 0; i < parts.length; i++) {
+    var parameter = parts[i].split('=');
+    parameters[parameter[0]] = parameter[1];
+  }
+  return parameters;
 })();
 
 (function () {
@@ -45,171 +45,171 @@ var scene;
 var vrMode = false;
 
 function bend( group, amount, multiMaterialObject ) {
-	function bendVertices( mesh, amount, parent ) {
-		var vertices = mesh.geometry.vertices;
+  function bendVertices( mesh, amount, parent ) {
+    var vertices = mesh.geometry.vertices;
 
-		if (!parent) {
-			parent = mesh;
-		}
+    if (!parent) {
+      parent = mesh;
+    }
 
-		for (var i = 0; i < vertices.length; i++) {
-			var vertex = vertices[i];
+    for (var i = 0; i < vertices.length; i++) {
+      var vertex = vertices[i];
 
-			// apply bend calculations on vertexes from world coordinates
-			parent.updateMatrixWorld();
+      // apply bend calculations on vertexes from world coordinates
+      parent.updateMatrixWorld();
 
-			var worldVertex = parent.localToWorld(vertex);
+      var worldVertex = parent.localToWorld(vertex);
 
-			var worldX = Math.sin( worldVertex.x / amount) * amount;
-			var worldZ = - Math.cos( worldVertex.x / amount ) * amount;
-			var worldY = worldVertex.y 	;
+      var worldX = Math.sin( worldVertex.x / amount) * amount;
+      var worldZ = - Math.cos( worldVertex.x / amount ) * amount;
+      var worldY = worldVertex.y  ;
 
-			// convert world coordinates back into local object coordinates.
-			var localVertex = parent.worldToLocal(new THREE.Vector3(worldX, worldY, worldZ));
-			vertex.x = localVertex.x;
-			vertex.z = localVertex.z+amount;
-			vertex.y = localVertex.y;
-		}
+      // convert world coordinates back into local object coordinates.
+      var localVertex = parent.worldToLocal(new THREE.Vector3(worldX, worldY, worldZ));
+      vertex.x = localVertex.x;
+      vertex.z = localVertex.z+amount;
+      vertex.y = localVertex.y;
+    }
 
-		mesh.geometry.computeBoundingSphere();
-		mesh.geometry.verticesNeedUpdate = true;
-	}
+    mesh.geometry.computeBoundingSphere();
+    mesh.geometry.verticesNeedUpdate = true;
+  }
 
-	for ( var i = 0; i < group.children.length; i ++ ) {
-		var element = group.children[ i ];
+  for ( var i = 0; i < group.children.length; i ++ ) {
+    var element = group.children[ i ];
 
-		if (element.geometry.vertices) {
-			if (multiMaterialObject) {
-				bendVertices( element, amount, group);
-			} else {
-				bendVertices( element, amount);
-			}
-		}
-	}
+    if (element.geometry.vertices) {
+      if (multiMaterialObject) {
+        bendVertices( element, amount, group);
+      } else {
+        bendVertices( element, amount);
+      }
+    }
+  }
 }
 
 function loadPano() {
-	panosList.then(function (panos) {
+  panosList.then(function (panos) {
 
-		panoCurrent = panos[counter];
-		var imgPano = panoCurrent.image;
-		var imgOverlay = panoCurrent.overlay;
+    panoCurrent = panos[counter];
+    var imgPano = panoCurrent.image;
+    var imgOverlay = panoCurrent.overlay;
 
-		// fade out current panorama.
-		new TWEEN.Tween(pano.material)
-			.to({opacity: 0}, 300)
-			.onComplete(function () {
-				// load in new panorama texture.
-				pano.material.map = THREE.ImageUtils.loadTexture(imgPano, THREE.UVMapping, fadeIn);
-			})
-			.start();
+    // fade out current panorama.
+    new TWEEN.Tween(pano.material)
+      .to({opacity: 0}, 300)
+      .onComplete(function () {
+        // load in new panorama texture.
+        pano.material.map = THREE.ImageUtils.loadTexture(imgPano, THREE.UVMapping, fadeIn);
+      })
+      .start();
 
-		// fade out current title.
-		new TWEEN.Tween(overlay.children[0].material)
-			.to({opacity: 0}, 300)
-			.onComplete(function () {
-				// load in new title.
-				overlay.children[0].material.map = THREE.ImageUtils.loadTexture(imgOverlay, THREE.UVMapping);
-			})
-			.start();
+    // fade out current title.
+    new TWEEN.Tween(overlay.children[0].material)
+      .to({opacity: 0}, 300)
+      .onComplete(function () {
+        // load in new title.
+        overlay.children[0].material.map = THREE.ImageUtils.loadTexture(imgOverlay, THREE.UVMapping);
+      })
+      .start();
 
-		// fade in newly loaded panorama.
-		function fadeIn() {
-			new TWEEN.Tween(pano.material)
-				.to({opacity: 1}, 1000)
-				.onComplete(fadeInOverlay)
-				.start();
-		}
+    // fade in newly loaded panorama.
+    function fadeIn() {
+      new TWEEN.Tween(pano.material)
+        .to({opacity: 1}, 1000)
+        .onComplete(fadeInOverlay)
+        .start();
+    }
 
-		// fade in newly loaded title.
-		function fadeInOverlay() {
-			new TWEEN.Tween(overlay.children[0].material)
-				.to({opacity: 1}, 300)
-				.start();
-		}
+    // fade in newly loaded title.
+    function fadeInOverlay() {
+      new TWEEN.Tween(overlay.children[0].material)
+        .to({opacity: 1}, 300)
+        .start();
+    }
 
-	});
+  });
 }
 
 
 // initialize scene
 function init() {
-	renderer = new THREE.WebGLRenderer({ antialias: true });
-	renderer.autoClear = false;
-	renderer.setClearColor( 0x000000 );
-	document.body.appendChild( renderer.domElement );
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.autoClear = false;
+  renderer.setClearColor( 0x000000 );
+  document.body.appendChild( renderer.domElement );
 
-	scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-	camera.position.z = 0.01; // set camera position so that OrbitControls works properly
-	scene.add(camera);
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+  camera.position.z = 0.01; // set camera position so that OrbitControls works properly
+  scene.add(camera);
 
-	// effect and controls for VR
-	effect = new THREE.VREffect(renderer);
-	vrControls = new THREE.VRControls(camera);
+  // effect and controls for VR
+  effect = new THREE.VREffect(renderer);
+  vrControls = new THREE.VRControls(camera);
 
-	// Fetch the JSON list of panos
-	function loadMaterial() {
-		return new Promise(function (resolve) {
-			var material = new THREE.MeshBasicMaterial({
-				side: THREE.DoubleSide,
-				transparent: true,
-				map: THREE.ImageUtils.loadTexture(
-					'images/background.jpg', // load placeholder rexture
-					THREE.UVMapping,
-					resolve
-				)
-			});
+  // Fetch the JSON list of panos
+  function loadMaterial() {
+    return new Promise(function (resolve) {
+      var material = new THREE.MeshBasicMaterial({
+        side: THREE.DoubleSide,
+        transparent: true,
+        map: THREE.ImageUtils.loadTexture(
+          'images/background.jpg', // load placeholder rexture
+          THREE.UVMapping,
+          resolve
+        )
+      });
 
-			pano = new THREE.Mesh( geometry, material );
-			pano.renderDepth = 2;
-			pano.rotation.set( 0, -90 * Math.PI / 180, 0 );
-			scene.add(pano);
-		});
-	}
-	panosList.then(loadMaterial).then(loadPano);
+      pano = new THREE.Mesh( geometry, material );
+      pano.renderDepth = 2;
+      pano.rotation.set( 0, -90 * Math.PI / 180, 0 );
+      scene.add(pano);
+    });
+  }
+  panosList.then(loadMaterial).then(loadPano);
 
-	// panorma mesh
-	var geometry = new THREE.SphereGeometry( 1000, 60, 60 );
-	geometry.applyMatrix( new THREE.Matrix4().makeScale( -1, 1, 1 ) );
+  // panorma mesh
+  var geometry = new THREE.SphereGeometry( 1000, 60, 60 );
+  geometry.applyMatrix( new THREE.Matrix4().makeScale( -1, 1, 1 ) );
 
-	// title text
-	overlay = new THREE.Object3D();
-	var mesh = new THREE.Mesh(
-		new THREE.PlaneGeometry( 63, 30, 20, 20 ),
-		new THREE.MeshBasicMaterial({
-			transparent: true,
-			alphaTest: 0.5,
-			side: THREE.FrontSide,
-			map: new THREE.TextureLoader().load('images/background-overlay.png')
-	}));
-	overlay.add( mesh );
-	overlay.position.set( 0, -3, -5 );
-	overlay.scale.set( 0.1, 0.1, 0.1 );
-	bend(overlay, 100);
-	mesh.renderOrder = 1;
-	scene.add( overlay );
+  // title text
+  overlay = new THREE.Object3D();
+  var mesh = new THREE.Mesh(
+    new THREE.PlaneGeometry( 63, 30, 20, 20 ),
+    new THREE.MeshBasicMaterial({
+      transparent: true,
+      alphaTest: 0.5,
+      side: THREE.FrontSide,
+      map: new THREE.TextureLoader().load('images/background-overlay.png')
+  }));
+  overlay.add( mesh );
+  overlay.position.set( 0, -3, -5 );
+  overlay.scale.set( 0.1, 0.1, 0.1 );
+  bend(overlay, 100);
+  mesh.renderOrder = 1;
+  scene.add( overlay );
 
-	// trigger function that begins to animate the scene.
-	new TWEEN.Tween()
-		.delay(400)
-		.start();
+  // trigger function that begins to animate the scene.
+  new TWEEN.Tween()
+    .delay(400)
+    .start();
 
-	// kick off animation
-	animate();
-	onWindowResize();
+  // kick off animation
+  animate();
+  onWindowResize();
 }
 
 function requestFullscreen() {
-	var el = renderer.domElement;
+  var el = renderer.domElement;
 
-	if (!isMobile()) {
-		effect.setFullScreen(true);
-		return;
-	}
+  if (!isMobile()) {
+    effect.setFullScreen(true);
+    return;
+  }
 
-	if (el.requestFullscreen) {
+  if (el.requestFullscreen) {
     el.requestFullscreen();
   } else if (el.mozRequestFullScreen) {
     el.mozRequestFullScreen();
@@ -219,67 +219,67 @@ function requestFullscreen() {
 }
 
 function onWindowResize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	if (vrMode) {
-		effect.setSize(window.innerWidth, window.innerHeight);
-	} else {
-		renderer.setSize(window.innerWidth, window.innerHeight);
-	}
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  if (vrMode) {
+    effect.setSize(window.innerWidth, window.innerHeight);
+  } else {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
 }
 
 function onFullscreenChange(e) {
-	var fsElement = document.fullscreenElement ||
+  var fsElement = document.fullscreenElement ||
     document.mozFullScreenElement ||
     document.webkitFullscreenElement;
 
-	if (!fsElement) {
+  if (!fsElement) {
     vrMode = false;
   } else {
-  	// lock screen if mobile
-  	window.screen.orientation.lock('landscape');
+    // lock screen if mobile
+    window.screen.orientation.lock('landscape');
   }
 }
 
 function onkey(e) {
-	panosList.then(function (panos) {
-		if (e.keyCode == '90') {
-			vrControls.zeroSensor();
-		} else if (e.keyCode == '37') { // left arrow - prev panorama
-			counter --;
-			if (counter < 0) {
-				counter = panos.length - 1;
-			}
-			loadPano();
-		} else if (e.keyCode == '39') { // right arrow - next panorama
-			counter ++;
-			if (counter == panos.length) {
-				counter = 0;
-			}
-			loadPano();
-		}
-	});
-	e.stopPropagation();
+  panosList.then(function (panos) {
+    if (e.keyCode == '90') {
+      vrControls.zeroSensor();
+    } else if (e.keyCode == '37') { // left arrow - prev panorama
+      counter --;
+      if (counter < 0) {
+        counter = panos.length - 1;
+      }
+      loadPano();
+    } else if (e.keyCode == '39') { // right arrow - next panorama
+      counter ++;
+      if (counter == panos.length) {
+        counter = 0;
+      }
+      loadPano();
+    }
+  });
+  e.stopPropagation();
 }
 
 function animate() {
-	TWEEN.update();
+  TWEEN.update();
 
-	if (vrMode) {
-		effect.render(scene, camera);
-	}  else {
-		renderer.render(scene, camera);
-	}
+  if (vrMode) {
+    effect.render(scene, camera);
+  }  else {
+    renderer.render(scene, camera);
+  }
 
-	vrControls.update();
+  vrControls.update();
 
-	requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 }
 
 document.querySelector('#enterVr').addEventListener('click', function() {
-	vrMode = vrMode ? false : true;
-	requestFullscreen();
-	onWindowResize();
+  vrMode = vrMode ? false : true;
+  requestFullscreen();
+  onWindowResize();
 });
 document.addEventListener('fullscreenchange', onFullscreenChange);
 document.addEventListener('mozfullscreenchange', onFullscreenChange);
